7af60f3ed363a11fb099370bffc5202a5f25c1d4
신다민 <damin8@naver.com>
2023-01-20 18:40:59 +0900

    [INIT] 초기설정
    - Basic Dependency 설정

---
+++ src/main/java/favian/devstuadmin/DevstuAdminApplication.java
@@ -0,0 +1,13 @@
+package favian.devstuadmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevstuAdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DevstuAdminApplication.class, args);
+    }
+
+}
---
+++ src/test/java/favian/devstuadmin/DevstuAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package favian.devstuadmin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevstuAdminApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
